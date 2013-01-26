663b631c68be635ce72c264f4851c42a0bfcd199
David Scott <dave.scott@eu.citrix.com>
2013-01-26 17:44:58 +0000

    Add capability negotiation logic

--- cli/client.ml
+++ cli/client.ml
@@ -12,4 +12,46 @@
  * GNU Lesser General Public License for more details.
  *)
 
-let watch copts () = ()
+let connect path =
+  Printf.fprintf stderr "Connecting to %s\n%!" path;
+  let sockaddr = Unix.ADDR_UNIX path in
+  let s = Unix.socket Unix.PF_UNIX Unix.SOCK_STREAM 0 in
+  Unix.connect s sockaddr;
+  Unix.in_channel_of_descr s, Unix.out_channel_of_descr s
+
+open Qmp
+
+let read (ic, _) =
+  let line = input_line ic in
+  Printf.fprintf stderr "READ [%s]\n%!" line;
+  message_of_string line
+
+let write (_, oc) m =
+  let msg = string_of_message m in
+  output_string oc msg;
+  output_string oc "\r\n";
+  flush oc
+
+let negotiate c =
+  match read c with
+  | Greeting { major; minor; micro; package } ->
+    Printf.fprintf stderr "Connected to qemu %d.%d.%d (%s)\n%!" major minor micro package;
+    write c (Command(None, Qmp_capabilities));
+    begin match read c with
+    | Success(None, Unit) ->
+      Printf.fprintf stderr "Capability negotiation complete\n%!";
+      ()
+    | x -> failwith (Printf.sprintf "Unexpected message: %s\n%!" (string_of_message x))
+    end
+  | x -> failwith (Printf.sprintf "Unexpected message: %s\n%!" (string_of_message x))
+
+let watch copts =
+  let c = connect copts.Common.socket in
+  negotiate c;
+
+  while true do
+    let m = read c in
+    Printf.fprintf stderr "%s\n%!" (string_of_message m);
+  done;
+  ()
+
